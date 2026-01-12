91c80a67456ff66c457c872636dac86fe0ac7c06
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-01-12 12:34:37 +0100

    test the composer et twig

--- App/public/index.php
+++ App/public/index.php
@@ -1,15 +1,21 @@
 <?php
-// 1. Charger l'autoloader de Composer
-require_once __DIR__ . '/../../vendor/autoload.php';
 
-// 2. Configurer où se trouvent tes fichiers HTML
-$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
-$twig = new \Twig\Environment($loader);
+require_once __DIR__ . '/../vendor/autoload.php';
+
+
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../templates');
+
+$twig = new \Twig\Environment($loader, [
+    // Optionnel : Désactiver le cache pour le développement pour voir les changements immédiatement
+    'cache' => false, 
+    'debug' => true,
+]);
 
 // 3. Essayer d'afficher une page
 try {
     echo $twig->render('test.html.twig', [
-        'message' => 'Félicitations ! Twig fonctionne correctement.'
+        'message' => 'Félicitations1 ! Twig fonctionne correctement1.',
+        'message2' => 'Félicitations2 ! Twig fonctionne correctement2.'
     ]);
 } catch (Exception $e) {
     echo "Erreur : " . $e->getMessage();
