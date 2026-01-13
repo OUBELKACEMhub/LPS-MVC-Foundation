5a8be7a8c1c73d7059be96466a36ae285a3f64bf
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-01-13 10:24:25 +0100

    tester twig

--- App/public/index.php
+++ App/public/index.php
@@ -1,33 +1,25 @@
 <?php
+// App/public/index.php
+
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../templates');
+
 $twig = new \Twig\Environment($loader, [
     'cache' => false, 
     'debug' => true
 ]);
 
-// 1. On définit l'utilisateur (Tableau associatif)
+// 3. Données de test
 $fakeUser = [
-    'name' => 'Ahmed',     // C'est cette valeur qu'on veut afficher
+    'name' => 'Ahmed',
     'points' => 450,
     'is_logged' => true
 ];
 
 try {
-    // 2. On l'envoie à la vue sous le nom 'user'
     echo $twig->render('test.html.twig', [
-        'message' => 'Test d\'affichage',
-        'user' => $fakeUser 
-    ]);
-} catch (Exception $e) {
-    echo "Erreur : " . $e->getMessage();
-}
-
-try {
-    // 2. On l'envoie à la vue sous le nom 'user'
-    echo $twig->render('base.html.twig', [
-        'message' => 'Test d\'affichage',
+        'nom' => 'Ahmed',
         'user' => $fakeUser 
     ]);
 } catch (Exception $e) {
