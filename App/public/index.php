c7a8b2da9be87bb9cd3cf4acf4f0f29e92adf4a4
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-01-18 18:02:10 +0100

    cree controller principale

--- App/public/index.php
+++ App/public/index.php
@@ -1,43 +1,51 @@
 <?php
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
-// use App\Core\Router;
-// use App\Core\RouterException;
-// use App\Controllers\AuthController;
-// use App\Controllers\DashboardController;
-
-// session_start();
-
-// $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../views'); 
-// $twig = new \Twig\Environment($loader, [
-//     'cache' => false, 
-//     'debug' => true
-// ]);
-
-// $authController = new AuthController($twig);
-// $dashboardController = new DashboardController($twig);
-
-// $router = new Router($_GET['url'] ?? '/');
-
-// // --- DÉFINITION DES ROUTES ---
-
-// $router->get('/', [$authController, 'showLogin']);
-
-// $router->post('/login', [$authController, 'login']);
-
-// $router->get('/logout', [$authController, 'logout']);
-
-// $router->get('/dashboard', [$dashboardController, 'index']);
+session_start();
+use App\src\Controllers\HomeController;
+use App\src\Core\Database;
+use App\src\Repositories\ProductRepository;
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 
-// try {
-//     $router->run();
-// } catch (RouterException $e) {
-//     header("HTTP/1.0 404 Not Found");
-//     echo $twig->render('404.html.twig', ['error' => $e->getMessage()]);
-// } catch (Exception $e) {
-//     echo "Erreur système : " . $e->getMessage();
-// }
+// 1. Config dial Twig
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../Views');
+$twig = new \Twig\Environment($loader, [
+    'cache' => false, 
+]);
 
-echo "hello";
+$request = $_SERVER['REQUEST_URI'];
+echo $request ;
+$route = str_replace('/Loyalty%20Points%20System/App/public', '', $request);
+$route = rtrim($route, '/');
+
+// 3. Wjjeh l-user l-Twig templates
+switch ($route) {
+    case '':
+    case '/':
+        $pdo = Database::getInstance()->getConnection();
+        $productRepo = new ProductRepository($pdo); 
+        $controller = new HomeController($twig, $productRepo); 
+        $controller->index();
+        break;
+
+    case '/login':
+        echo $twig->render('login.html.twig', [
+            'title' => 'Contactez-nous'
+        ]);
+        break;
+
+    case '/cart':
+        echo $twig->render('cart.html.twig', [
+            'title' => 'cart'
+        ]);
+        break; 
+    case '/dashboard':
+        echo $twig->render('userDashboard.html.twig', [
+            'title' => 'user Dashboard'
+        ]);
+        break;       
+
+    default:
+        http_response_code(404);
+        echo $twig->render('404.html.twig');
+        break;
+}
